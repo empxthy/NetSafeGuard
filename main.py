c4ef0c908b11aba5ebd0f32d9970d984ebc53db3
alexemployed <landshok35@gmail.com>
2023-12-23 18:41:29 +0400

    Release v1.0.0

---
+++ main.py
@@ -0,0 +1,133 @@
+#!/usr/bin/python
+
+# Coding: UTF-8
+# Name: NetSafeGuard
+# Author: AlexEmployed
+# Version: 1.0.0
+# License: GPL-3.0 version
+# Copyright: alexemployed 2023
+# Github: https://github.com/alexemployed
+# Language: Python
+
+# Imports
+import asyncio
+import configparser
+import subprocess
+import os
+import re
+from collections import namedtuple
+
+# Colors
+_black = "\033[0;30m"
+_red = "\033[0;31m"
+_green = "\033[0;32m"
+_brown = "\033[0;33m"
+_blue = "\033[0;34m"
+_yellow = "\033[1;33m"
+_purple = "\033[0;35m"
+_cyan = "\033[0;36m"
+_white="\033[0;37m"
+_lightGray = "\033[0;37m"
+_darkGray = "\033[1;30m"
+_lightRed = "\033[1;31m"
+_lightGreen = "\033[1;32m"
+_lightBlue = "\033[1;34m"
+_lightPurple = "\033[1;35m"
+_lightCyan = "\033[1;36m"
+_lightWhite = "\033[1;37m"
+
+
+# Privalages
+    
+def check_root():
+    ret = 0
+    if os.geteuid != 0:
+        msg = "[sudo] password for %u: "
+        ret = subprocess.check_call("sudo -v -p '%s'" %msg, shell=True)
+    return ret
+
+def check_admin():
+    try:
+        subprocess.check_call(["net", "session"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    
+
+# Windows Wifi
+
+def get_windows_saved_ssids():
+    output = subprocess.check_output("netsh wlan show profiles").decode()
+    ssids = []
+    profiles = re.findall(r"Entire user profile\s(.*)", output)
+    for profile in profiles:
+        ssid = profile.strip().strip(":").strip()
+        ssids.append(ssid)
+    return ssids
+
+def get_windows_saved_wifi_passwords(verbose=1):
+    ssids = get_windows_saved_ssids()
+    Profile = namedtuple("Profile", ["ssid", "ciphers", "key"])
+    profiles = []
+    for ssid in ssids:
+        ssid_details = subprocess.check_output(f"""netsh wlan show profile "{ssid}" key=clear""").decode()
+        ciphers = re.findall(r"Cipher\s(.*)", ssid_details)
+        ciphers = "/".join([c.strip().strip(":").strip() for c in ciphers])
+        key = re.findall(r"Key Content\s(.*)", ssid_details)
+        try:
+            key = key[0].strip().strip(":").strip()
+        except IndexError:
+            key = "None"
+        profile = Profile(ssid=ssid, ciphers=ciphers, key=key)
+        if verbose >= 1:
+            print_windows_profile(profile)
+        profiles.append(profile)
+    return profiles
+
+def print_windows_profile(profile):
+    print(f"{profile.ssid:25}{profile.ciphers:15}{profile.key:50}")
+
+def print_windows_profiles(verbose):
+    print("SSID                     CIPHER(S)      KEY")
+    get_windows_saved_wifi_passwords(verbose)
+
+# Linux Wifi
+
+def get_linux_saved_wifi_passwords(verbose=1):   
+    network_connections_path = "/etc/NetworkManager/system-connections/"
+    fields = ["ssid", "auth-alg", "key-mgmt", "psk"]
+    Profile = namedtuple("Profile", [f.replace("-", "_") for f in fields])
+    profiles = []
+    for file in os.listdir(network_connections_path):
+        data = { k.replace("-", "_"): None for k in fields }
+        config = configparser.ConfigParser()
+        config.read(os.path.join(network_connections_path, file))
+        for _, section in config.items():
+            for k, v in section.items():
+                if k in fields:
+                    data[k.replace("-", "_")] = v
+        profile = Profile(**data)
+        if verbose >= 1:
+            print_linux_profile(profile)
+        profiles.append(profile)
+    return profiles
+
+def print_linux_profile(profile):
+    print(f"{str(profile.ssid):25}{str(profile.auth_alg):5}{str(profile.key_mgmt):10}{str(profile.psk):50}")
+
+def print_linux_profiles(verbose):
+    print("SSID                     AUTH KEY-MGMT  PSK")
+    get_linux_saved_wifi_passwords(verbose)
+    
+    
+def print_profiles(verbose=1):
+    if os.name == "nt":
+        print_windows_profiles(verbose)
+    elif os.name == "posix":
+        print_linux_profiles(verbose)
+    else:
+        raise NotImplemented("Program runs only on Windows and POSIX systems!")
+    
+    
+if __name__ == "__main__":
+    print_profiles()
